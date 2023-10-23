cd18d0d37101d332478e91167533088935a25d61
Joseda Rios <josriolop@gmail.com>
2023-10-23 02:12:16 +0200

    [fix-ec2] Fix EC2 tf configuration
    Signed-off-by: Joseda Rios <josriolop@gmail.com>

--- iac/deployment/main.tf
+++ iac/deployment/main.tf
@@ -15,8 +15,7 @@ module "wb_network" {
 resource "aws_instance" "wb_ec2" {
   ami                    = var.ami_id
   instance_type          = "t3.micro"
-#  vpc_security_group_ids = [aws_security_group.wb_sg.id]
-  security_groups        = [module.wb_network.security_group_id]
+  vpc_security_group_ids = [module.wb_network.security_group_id]
   subnet_id              = module.wb_network.subnet_id
 
   tags = {
